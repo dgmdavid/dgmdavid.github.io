836f4bceed7e7d89eb210b13fcb129e639de9261
David G. Maziero <dgmdavid@gmail.com>
2016-03-15 00:46:19 -0300

    added deposito

---
+++ deposito/deposito.js
@@ -0,0 +1,11 @@
+﻿// Funções JAVASCRIPT
+// by DGM
+
+//******************************
+
+function pu( a, b ) {
+ document.write('<TR> <TD Style="background-color:silver">');
+ document.write('<A HREF="x/'+a+'" TARGET="_blank">'+a+'</A>' );
+ document.write('</TD><TD Style="background-color:#DDDDDD"> '+b+' </TD></TR>');
+ return 0;
+}
